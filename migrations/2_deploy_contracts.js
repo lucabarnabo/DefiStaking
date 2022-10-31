ed27ce171da9aa5f4a46bdc2bae68650889f2cd9
Luca Barnabo <lucabarnabo@yahoo.com.ar>
2022-10-31 20:45:04 -0300

    minor fixes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
-/* const Tether = artifacts.require("Tether"); */
+const Tether = artifacts.require("Tether");
 const Latam = artifacts.require("Latam");
+const DescentralBank = artifacts.require("DescentralBank");
 
 module.exports = async function(deployer) {
-  /* await deployer.deploy(Tether); */
+  await deployer.deploy(Tether);
   await deployer.deploy(Latam);
+  await deployer.deploy(DescentralBank);
 };
